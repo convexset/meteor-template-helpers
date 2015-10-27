eded122850927bcc22607b35b53e4a3624224527
Jeremy Chen <convexset@gmail.com>
2015-10-27 22:09:35 +0800

    Added uniq (based on deep compare) to cover up a mess up (see pre. commit) that led to 0.1.8 being useless. (Actually a pointless version bump.)

--- packages/template-helpers/package.js
+++ packages/template-helpers/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'convexset:template-helpers',
-	version: '0.1.8',
+	version: '0.1.9',
 	summary: 'Template-level manipulation tools (such as python-style \"enumerate\" for collections and \"logic\").',
 	git: 'https://github.com/convexset/meteor-template-helpers',
 	documentation: '../../README.md'
