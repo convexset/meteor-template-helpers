27749b6ab20cab567afe1c8ea290bfcd4955ff46
Jeremy Chen <convexset@gmail.com>
2016-11-11 11:33:00 +0800

    new version complete: bump version

--- packages/template-helpers/package.js
+++ packages/template-helpers/package.js
@@ -1,13 +1,13 @@
 Package.describe({
 	// [validatis:stack]
 	name: 'convexset:template-helpers',
-	version: '0.1.19_1',
+	version: '0.1.20',
 	summary: 'Template-level manipulation tools (such as python-style \"enumerate\" for collections and \"logic\").',
 	git: 'https://github.com/convexset/meteor-template-helpers',
 	documentation: '../../README.md'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function setup(api) {
 	api.versionsFrom('1.2.0.2');
 	api.use([
 		'ecmascript', 'blaze', 'ejson', 'templating',
@@ -27,7 +27,7 @@ Package.onUse(function(api) {
 	api.export(['TemplateHelpers'], 'client');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function setup(api) {
 	api.use(['tinytest', 'test-helpers'], 'client');
 	api.use([
 		'ecmascript', 'mongo', 'tracker', 'jquery',
